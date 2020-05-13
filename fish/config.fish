72d74742f27547207cb57e73a90ca468c0ce58b0
Pateros Petros <el19826@mail.ntua.gr>
2020-05-13 11:15:20 +0300

    Updates

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 #set -U fish_greeting
 #fish_vi_key_bindings
 #set -U fish_prompt_pwd_dir_length 0
+#set -U fish_escape_delay_ms 10
 
 #set -U fish_color_user '00D7FF'
 #set -U fish_color_host 'FFF03D'
@@ -8,6 +9,7 @@
 #set -U fish_color_cwd '00FF50'
 
 alias mpvs='mpv --input-ipc-server=/tmp/mpvsocket'
+abbr YamahaMD xfce4-appfinder
 
 abbr cdl 'cd ~/.local'
 abbr cdc 'cd ~/.local/etc'
@@ -18,5 +20,6 @@ abbr p pacman
 abbr P 'sudo pacman'
 abbr Startx 'startx 2>> /tmp/startx.log'
 abbr Compile 'make clean && make && sudo make install && make clean'
+abbr KillTEAMS 'pkill "^teams\$"'
 
 
